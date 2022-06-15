848f7c2bb6682fed45e74e81e02808746061625f
Tuhacrt <a067800497@gmail.com>
2022-06-15 15:24:08 +0800

    Update 2022-06-15T07:23:59.504Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,8 @@
+"use strict";
+
 console.log("Hello!");
 console.log("GGG");
-
-$(document).ready(() => {
+$(document).ready(function () {
   console.log("HesSchool Hello!");
 });
+//# sourceMappingURL=all.js.map
